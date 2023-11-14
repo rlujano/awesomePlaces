7425066f645550980515ab68743d9c2ce2dd45fa
Rene Lujano <rlujanol@nttdata.com>
2023-11-14 12:35:25 -0500

    Added initial commit with hello world

---
+++ AwesomePlacesUITests/AwesomePlacesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AwesomePlacesUITestsLaunchTests.swift
+//  AwesomePlacesUITests
+//
+//  Created by Rene Lujano Laura on 13/11/23.
+//
+
+import XCTest
+
+final class AwesomePlacesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
